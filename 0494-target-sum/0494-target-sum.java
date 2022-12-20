4b8ac0176551607827f1600eb51cd6949406cb97
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2022-12-20 20:21:24 +0530

    Time: 9 ms (86.11%), Space: 43.3 MB (43.80%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,31 +1,28 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
+    public int findTargetSumWays(int[] nums, int targ) {
         int sum=0;
         for (int i=0;i<nums.length;i++) sum+=nums[i];
-        int tar=(sum-target)/2;
-        if (sum-target<0 || (sum-target)%2!=0) return 0;
+        
+        int tar=(sum-targ)/2;
+        if (sum-targ<0 || (sum-targ)%2!=0) return 0;
+        
         int[][] dp=new int[nums.length][tar+1];
-      for (int i=0;i<nums.length;i++){
-            for (int j=0;j<tar+1;j++){
-                dp[i][j]=-1;
+        if (nums[0]==0) dp[0][0]=2;
+        else dp[0][0]=1;
+        
+        if (nums[0]!=0 && nums[0]<=tar) dp[0][nums[0]]=1;
+        // for (int i=0;i<nums.length;i++) dp[i][0]=1;
+        
+        for (int ind=1;ind<nums.length;ind++){
+            for (int target=0;target<=tar;target++){
+                int unTake=dp[ind-1][target];
+                int take=0;
+                if (target>=nums[ind]) take=dp[ind-1][target-nums[ind]];
+                dp[ind][target]=unTake+take;     
             }
         }
-        return recurse(nums.length-1, tar, nums,dp);
+        return dp[nums.length-1][tar];
 
     }
-    static int recurse(int ind,int target,int[] nums,int[][] dp){
-        // if (target==0) return 1;
-        if (ind==0){
-            if (target==0 && nums[ind]==0) return 2;
-            if (target==0 || nums[ind]==target) return 1;
-            else return 0;
-        };
-        if (dp[ind][target]!=-1) return dp[ind][target];
-        int unTake=recurse(ind-1,target,nums,dp);
-        int take=0;
-        if (target>=nums[ind]) take=recurse(ind-1, target-nums[ind], nums,dp);
-        dp[ind][target]=unTake+take;
-        return unTake+take;
 
-    }
 }
