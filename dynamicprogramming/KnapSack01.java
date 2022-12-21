13464b75caca38b5d3e4df271b11f947e0ffe8eb
dhanush <vellasiridhanush@gmail.com>
2022-12-21 21:19:28 +0530

    added problem

---
+++ dynamicprogramming/KnapSack01.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+
+// LINK->https://www.codingninjas.com/codestudio/problem-details/0-1-knapsack_920542
+
+
+// public class KnapSack01 {
+//     public static void main(String[] args) {
+//         int bagWeight=5;
+//         int[] weights={3,2,5};
+//         int[] value={30,50,60};
+//         int[][] dp=new int[weights.length][bagWeight+1];
+//         System.out.println(recurse(2, bagWeight,weights,value,dp));
+//         System.out.println(Arrays.deepToString(dp));
+//     }
+//     static int recurse(int ind,int bagWeight,int[] weights,int[] value,int[][] dp){
+
+//         if (ind==0){
+//             if (bagWeight-weights[ind]>=0) return value[ind]; 
+//             return 0;
+//         }
+//         if (bagWeight<=0) return 0;
+//         if (dp[ind][bagWeight]!=0) return dp[ind][bagWeight];
+
+//             int unTake=recurse(ind-1, bagWeight,weights,value,dp);
+//             int take=0;
+//             if (bagWeight-weights[ind]>=0){
+//                 take=value[ind]+recurse(ind-1, bagWeight-weights[ind], weights, value,dp);
+//             }
+//             dp[ind][bagWeight]=Math.max(take, unTake);
+//             return Math.max(take,unTake);
+
+         
+
+//     }
+// }
+
+
+public class KnapSack01 {
+    public static void main(String[] args) {
+        int bagWeight=5;
+        int[] weights={3,2,5};
+        int[] value={30,50,60};
+        int[][] dp=new int[weights.length][bagWeight+1];
+        if (bagWeight-weights[0]>=0) dp[0][weights[0]]=value[0];
+        for (int ind=1;ind<weights.length;ind++){
+            for (int j=0;j<=bagWeight;j++){
+                int unTake=dp[ind-1][bagWeight];
+                int take=0;
+                if (bagWeight-weights[ind]>=0){
+                    take=value[ind]+dp[ind-1][bagWeight-weights[ind]];
+                }
+                dp[ind][bagWeight]=Math.max(take, unTake);   
+            }
+        }
+        System.out.println(Arrays.deepToString(dp));
+
+        System.out.println(dp[weights.length-1][bagWeight]);
+    }
+ 
+}
