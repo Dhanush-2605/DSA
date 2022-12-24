9caac469d1eff85cf4a38501f712d961a6b25924
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2022-12-24 19:19:41 +0530

    Time: 137 ms (10.70%), Space: 42.7 MB (78.34%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -5,24 +5,29 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
 
         int[][] dp=new int[coins.length][amount+1];
-     for(int row[]: dp)
-      Arrays.fill(row,-1);
-        int ans=recurse(coins.length-1,amount,coins,dp);
-          if(ans >= (int)Math.pow(10,9)) return -1;
-    return ans;
+        for (int i=0;i<=amount;i++){
+            if (i%coins[0]==0){
+
+            dp[0][i]=i/coins[0];
+            }
+            else dp[0][i]=(int)Math.pow(10,9);
+        };
+        
+        
+        
+        for (int ind=1;ind<coins.length;ind++){
+            for (int target=0;target<=amount;target++){
+           int unTake=0+dp[ind-1][target];
+           int take=(int)Math.pow(10,9);
+            if (target>=coins[ind]) take=1+dp[ind][target-coins[ind]];
+            dp[ind][target]=Math.min(take,unTake);             
+            }
+        }
+        int res=dp[coins.length-1][amount];
+        System.out.println(res);
+        if (res>=(int)Math.pow(10,9)) return -1;
+        else return res;
         
     }
-    int recurse(int ind,int target,int[] coins,int[][] dp){
-        if (ind==0){
-            if (target%coins[ind]==0) return target/coins[ind];
-            else return (int)Math.pow(10,9);
-            
-        }
-        if (dp[ind][target]!=-1) return dp[ind][target];
-        int unTake=0+recurse(ind-1,target,coins,dp);
-        int take=(int)Math.pow(10,9);
-        if (target>=coins[ind]) take=1+recurse(ind,target-coins[ind],coins,dp);
-        dp[ind][target]=Math.min(take,unTake);
-        return Math.min(take,unTake);
-    }
+
 }
