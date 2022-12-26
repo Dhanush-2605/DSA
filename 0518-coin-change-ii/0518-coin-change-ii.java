9d23048ee9e271083597e92f32e4ea5c1be20966
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2022-12-26 10:58:42 +0530

    Time: 168 ms (6.57%), Space: 66.3 MB (5.08%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,32 +2,28 @@ class Solution{
 public int change(int amount, int[] coins) {
         
         int[][] dp = new int[coins.length][amount+1] ;
-        for(int i=0 ; i<coins.length ; i++){
-            Arrays.fill ( dp[i] , -1);
-        }
-        
-        return helper(coins , amount , coins.length-1 , dp);
+    
+   for (int i=0;i<coins.length;i++) dp[i][0]=1;
+    for (int i=0;i<=amount;i++){
+        if (i%coins[0]==0) dp[0][i]=1;
     }
     
-    private int helper(int[] coins , int amount, int i , int[][] dp) {
-        if(amount == 0) return 1;
-        
-        if(i == 0){
-            if(amount % coins[0] == 0) return 1;
-            else return 0;
-        } 
-        
-        if(dp[i][amount] != -1) return dp[i][amount];
-        
+    for (int ind=1;ind<coins.length;ind++){
+        for (int target=1;target<=amount;target++){
+        int notPick = dp[ind-1][target];   
         int pick = 0;
-        if(amount >= coins[i]){
-            pick = helper(coins , amount - coins[i] , i , dp);
+        if(target >= coins[ind]){
+            pick = dp[ind][target - coins[ind]];
+        }       
+         
+        dp[ind][target] = pick + notPick;
+            
         }
-        
-        int notPick = helper(coins , amount , i-1 , dp);
-        
-        dp[i][amount] = pick + notPick;
-        
-        return dp[i][amount];
     }
+       System.out.println(Arrays.deepToString(dp));
+        
+        return dp[coins.length-1][amount];
+    }
+    
+    
 }
