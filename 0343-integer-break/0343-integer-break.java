00aea042e24e8b18cef459443989357b533065db
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2022-12-28 10:22:33 +0530

    Time: 2 ms (38.03%), Space: 39.4 MB (72.54%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -2,21 +2,18 @@ class Solution {
     public int integerBreak(int n) {
         if (n==2 || n==3)return n-1;
         int[][] dp=new int[n][n+1];
-        for (int[] row:dp) Arrays.fill(row,-1);
-        return recurse(n-1,n,dp);
-        
-    }
-    int recurse(int ind,int target,int[][] dp){
-        if (target==0) return 0;
-        if (ind==1){
-            if (target>=ind) return target;
+        for (int i=1;i<=n;i++) dp[1][i]=i;
+        for (int ind=2;ind<n;ind++){
+            for (int target=1;target<=n;target++){
+              int unTake=dp[ind-1][target];
+              int take=0;
+            if(target>=ind) take=ind*dp[ind][target-ind];  
+            dp[ind][target]=Math.max(take,unTake);           
+            }
+            
         }
-        if (dp[ind][target]!=-1) return dp[ind][target];
-        int unTake=recurse(ind-1,target,dp);
-        int take=0;
-        if(target>=ind) take=ind*recurse(ind,target-ind,dp);
-        dp[ind][target]=Math.max(take,unTake);
-        return Math.max(take,unTake);
+        return dp[n-1][n];
         
     }
+
 }
