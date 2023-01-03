df91b7441af6d5281019a8aa20bd174db4083c5a
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-01-03 10:37:40 +0530

    Time: 50 ms (7.12%), Space: 50.3 MB (36.15%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,17 +1,21 @@
 class Solution {
+    static int prime =(int)(Math.pow(10,9)+7);
+
     public int numDistinct(String s, String t) {
         int n1=s.length();
         int n2=t.length();
-        int[][] dp=new int[n1][n2];
-        for (int[] row:dp) Arrays.fill(row,-1);
-        return recurse(n1-1,n2-1,s,t,dp);
+        int[][] dp=new int[n1+1][n2+1];
+        for (int i=0;i<n1+1;i++) dp[i][0]=1;
+        for (int i=1;i<n2+1;i++) dp[0][i]=0;
+        
+        for (int i=1;i<n1+1;i++){
+            for (int j=1;j<n2+1;j++){
+              if (s.charAt(i-1)==t.charAt(j-1)) dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])%prime;
+              else dp[i][j]=dp[i-1][j];             
+            }
+        }
+        return dp[n1][n2];
         
     }
-    int recurse(int i,int j,String s,String t,int[][] dp){
-        if(j<0) return 1;
-        if (i<0) return 0;
-        if (dp[i][j]!=-1) return dp[i][j];
-        if (s.charAt(i)==t.charAt(j)) return dp[i][j]=recurse(i-1,j-1,s,t,dp)+recurse(i-1,j,s,t,dp);
-        else return dp[i][j]=recurse(i-1,j,s,t,dp);
-    }
+
 }
