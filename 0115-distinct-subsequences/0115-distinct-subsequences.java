5168ccfbd24aaf82a04876e869c3f09124116b6c
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-01-03 10:19:13 +0530

    Time: 31 ms (44.07%), Space: 49.5 MB (69.93%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n1=s.length();
+        int n2=t.length();
+        int[][] dp=new int[n1][n2];
+        for (int[] row:dp) Arrays.fill(row,-1);
+        return recurse(n1-1,n2-1,s,t,dp);
+        
+    }
+    int recurse(int i,int j,String s,String t,int[][] dp){
+        if(j<0) return 1;
+        if (i<0) return 0;
+        if (dp[i][j]!=-1) return dp[i][j];
+        if (s.charAt(i)==t.charAt(j)) return dp[i][j]=recurse(i-1,j-1,s,t,dp)+recurse(i-1,j,s,t,dp);
+        else return dp[i][j]=recurse(i-1,j,s,t,dp);
+    }
+}
