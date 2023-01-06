f625f7c831816a7445da01eeb2d4d5dd37055bbe
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-01-06 18:16:31 +0530

    Time: 24 ms (77.12%), Space: 44.6 MB (56.69%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,29 +1,37 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int i = s.length();
-        int j = p.length();
-        
-        boolean[][] dp = new boolean[i+1][j+1];
-        
-        dp[0][0] = true;
-        
-        for (int k = 1; k < j + 1; k++) {
-            if (p.charAt(k - 1) == '*') {
-                dp[0][k] = dp[0][k-1];
+        int n1=s.length();
+        int n2=p.length();
+        int[][] dp=new int[n1][n2];
+        for (int[] row:dp) Arrays.fill(row,-1);
+        if (recurse(n1-1,n2-1,s,p,dp)==1)return true; 
+        return false;    
+    }
+    int recurse(int n1,int n2,String s,String p,int[][] dp){
+        if (n1<0 && n2<0) return 1;
+        if (n2<0 && n1>=0) return 0;
+        if (n1<0 && n2>=0){
+            for (int i=0;i<=n2;i++){
+                if (p.charAt(i)!='*') return 0;
             }
+         return 1;
+         }
+         if (dp[n1][n2]!=-1) return dp[n1][n2];
+
+
+
+        if  (s.charAt(n1)==p.charAt(n2) || p.charAt(n2)=='?') return recurse(n1-1,n2-1,s,p,dp);
+          if (p.charAt(n2)=='*'){
+              int r1=recurse(n1-1,n2,s,p,dp);
+              int r2=recurse(n1,n2-1,s,p,dp);
+              if (r1==0 && r2==0) return dp[n1][n2]=0;
+              else return dp[n1][n2]=1;
+            
+            
         }
+        return dp[n1][n2]=0;
         
         
-        for(int x = 1 ; x <= i ; x++)
-        {
-            for(int y = 1 ; y <= j ; y++){
-                if(s.charAt(x-1) == p.charAt(y-1) || p.charAt(y-1) == '?')   
-                    dp[x][y] = dp[x-1][y-1];
-                else if(p.charAt(y-1) == '*')   
-                    dp[x][y] = (dp[x-1][y] | dp[x][y-1]);
-            }
-        }
-        
-        return dp[i][j];
+
     }
 }
