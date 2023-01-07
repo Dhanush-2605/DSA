da77c1c15f90e1bc89721e96bdd2ef95018c45d7
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-01-07 18:37:34 +0530

    Time: 1 ms (93.06%), Space: 42 MB (96.31%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,21 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp=new int[prices.length][2];
-        for(int[] row:dp) Arrays.fill(row,-1);
-        return recurse(0,1,prices,dp);
+        int[][] dp=new int[prices.length+1][2];
+        int len=prices.length;
+        dp[len][0]=0;
+        dp[len][1]=0;
+        for (int ind=len-1;ind>=0;ind--){
+              for (int buy=0;buy<=1;buy++){
+             int profit=0;
+             if (buy==1){
+                 profit=Math.max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
+             }
+            else{
+                profit=Math.max(prices[ind]+dp[ind+1][1],dp[ind+1][0]);
+        }
+         dp[ind][buy]=profit;
+                
+            }
+        }
+        return dp[0][1];
         
     }
-    int recurse(int ind,int buy,int[] prices,int[][] dp){
-        if (ind==prices.length) return 0;
-        int profit=0;
-        if (dp[ind][buy]!=-1) return dp[ind][buy];
-        if (buy==1){
-            profit=Math.max(-prices[ind]+recurse(ind+1,0,prices,dp),recurse(ind+1,1,prices,dp));
-            
-        }
-        else{
-            profit=Math.max(prices[ind]+recurse(ind+1,1,prices,dp),recurse(ind+1,0,prices,dp));
-        }
-        return dp[ind][buy]=profit;
-    }
+
 }
