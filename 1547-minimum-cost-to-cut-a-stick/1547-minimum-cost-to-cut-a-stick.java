6df43bc94fb332b92d7afeefcf6104bec63030af
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-01-16 18:31:04 +0530

    Time: 13 ms (85.67%), Space: 42.1 MB (65.11%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -3,24 +3,26 @@ class Solution {
         int[] cutArr=new int[cuts.length+2];
         Arrays.sort(cuts);
         int len=cuts.length;
-        int[][] dp=new int[len+1][len+1];
-        for (int[] row:dp) Arrays.fill(row,-1);
+        System.out.println(len);
+        int[][] dp=new int[len+2][len+2];
         for (int i=1;i<=cuts.length;i++){
             cutArr[i]=cuts[i-1];
         }
         cutArr[cuts.length+1]=n;
         System.out.println(Arrays.toString(cutArr));
-        return recurse(1,cuts.length,cutArr,dp);
+        for (int i=len;i>=1;i--){
+            for (int j=1;j<=len;j++){
+                if (i>j) continue;
+                int min=Integer.MAX_VALUE;
+                for (int ind=i;ind<=j;ind++){
+                    int cost=cutArr[j+1]-cutArr[i-1]+dp[i][ind-1]+dp[ind+1][j];
+                    min=Math.min(cost,min);
+                  }
+              dp[i][j]=min;           
+            }
+        }
+        return dp[1][cuts.length];
         
     }
-    int recurse(int i,int j,int[] cuts,int[][] dp){
-        if (i>j) return 0;
-        if (dp[i][j]!=-1) return dp[i][j];
-        int min=Integer.MAX_VALUE;
-        for (int ind=i;ind<=j;ind++){
-            int cost=cuts[j+1]-cuts[i-1]+recurse(i,ind-1,cuts,dp)+recurse(ind+1,j,cuts,dp);
-            min=Math.min(cost,min);
-        }
-        return dp[i][j]=min;
-    }
+
 }
