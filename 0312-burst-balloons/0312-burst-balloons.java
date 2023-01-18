33f7305f33811464a670e131b1d82d6e08b8b85a
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-01-18 19:08:00 +0530

    Time: 99 ms (62.48%), Space: 41.9 MB (77.91%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,24 +1,25 @@
 class Solution {
     public int maxCoins(int[] nums) {
         int len=nums.length;
-        int[][] dp=new int[len+1][len+1];
-        for (int[] row:dp) Arrays.fill(row,-1);
+        int[][] dp=new int[len+2][len+2];      
         ArrayList<Integer> l=new ArrayList<Integer>();
         for (int i:nums) l.add(i);
         l.add(0,1);
         l.add(len+1,1);
-        // Collections.sort(l);
-        return recurse(1,len,l,dp);
+        for (int i=len;i>=1;i--){
+            for (int j=1;j<=len;j++){
+                if (i>j) continue;
+                int max=Integer.MIN_VALUE;
+                for (int k=i;k<=j;k++){
+                    int steps=l.get(i-1)*l.get(k)*l.get(j+1) + dp[i][k-1]+dp[k+1][j];
+                    max=Math.max(max,steps);
+        }
+         dp[i][j]=max;              
+            }
+        }
+    
+        return dp[1][len];
         
     }
-    int recurse(int i,int j,ArrayList<Integer> nums,int[][] dp){
-        if (i>j) return 0;
-        if (dp[i][j]!=-1) return dp[i][j];
-        int max=Integer.MIN_VALUE;
-        for (int k=i;k<=j;k++){
-            int steps=nums.get(i-1)*nums.get(k)*nums.get(j+1) + recurse(i,k-1,nums,dp)+recurse(k+1,j,nums,dp);
-            max=Math.max(max,steps);
-        }
-        return dp[i][j]=max;
-    }
+
 }
