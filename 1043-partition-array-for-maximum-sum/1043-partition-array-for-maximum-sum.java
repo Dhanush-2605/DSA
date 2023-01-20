060937b4824f2a9f6cb2b4ed87b4b65eeea6f7b9
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-01-20 19:07:25 +0530

    Time: 5 ms (96.60%), Space: 41.5 MB (94.81%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,25 +1,23 @@
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n=arr.length;
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        return recurse(0,n,arr,k,dp);
-        
-    }
-    int recurse(int ind,int n,int[] arr,int k,int[] dp){
-        if (ind>=n) return 0;
-        if (dp[ind]!=-1) return dp[ind];
-        int max=Integer.MIN_VALUE;
-        int len=0;
-        int maxRes=Integer.MIN_VALUE;
-        for (int i=ind;i<Math.min(ind+k,n);i++){
-            len++;
-            max=Math.max(max,arr[i]);
-            int sum=(len)*max+recurse(i+1,n,arr,k,dp);
-            maxRes=Math.max(maxRes,sum);
+        int[] dp=new int[n+1];    
+        for (int ind=n-1;ind>=0;ind--){
+            int max=Integer.MIN_VALUE;
+            int len=0;
+            int maxRes=Integer.MIN_VALUE;
+            for (int i=ind;i<Math.min(ind+k,n);i++){
+                len++;
+                max=Math.max(max,arr[i]);
+                int sum=(len)*max+dp[i+1];
+                maxRes=Math.max(maxRes,sum);
             
             
         }
-        return dp[ind]=maxRes;
+         dp[ind]=maxRes;      
+        }
+        return dp[0];
+        
     }
+
 }
