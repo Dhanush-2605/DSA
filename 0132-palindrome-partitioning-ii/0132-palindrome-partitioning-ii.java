a91728ff1c7fee2811ebb3f8a1b49cd945ac2b3d
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-01-24 15:34:52 +0530

    Time: 1360 ms (23.04%), Space: 40 MB (97.01%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,22 +1,20 @@
 class Solution {
     public int minCut(String s) {
         int len=s.length();
-        int[] dp=new int[len];
-        Arrays.fill(dp,-1);
-        return recurse(0,len,s,dp)-1;
-    }
-    int recurse(int ind,int j,String s,int[] dp){
-        if (ind>=j) return 0;
-        if (dp[ind]!=-1) return dp[ind];
+        int[] dp=new int[len+1];
+        for (int ind=len-1;ind>=0;ind--){
         int minCuts=Integer.MAX_VALUE;
-        for (int k=ind;k<j;k++){
+        for (int k=ind;k<len;k++){
             if (isPalindrome(ind,k,s)==true){
                 
-            int cuts=1+recurse(k+1,j,s,dp);
+            int cuts=1+dp[k+1];
             minCuts=Math.min(minCuts,cuts);
             }
         }
-        return dp[ind]=minCuts;
+        dp[ind]=minCuts;
+            
+        }
+        return dp[0]-1;
     }
     boolean isPalindrome(int s,int e,String str){
         while (e>=s){
