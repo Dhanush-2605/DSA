a3b5f323a06791abeba2616f58e5849064070766
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-02-03 16:07:17 +0530

    Time: 2 ms (80.98%), Space: 39.1 MB (80.98%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int totalNQueens(int n) {
+    boolean[][] board=new boolean[n][n];
+     return placeQueen(board,0);   
+    }
+    int placeQueen(boolean[][] board,int row){
+        if (row==board.length) return 1;
+        int count=0;
+        for (int col=0;col<board.length;col++){
+            if (isSafe(board,row,col)){
+                board[row][col]=true;
+                count+=placeQueen(board,row+1);
+                board[row][col]=false;
+            }
+        }
+        return count;
+    }
+    boolean isSafe(boolean[][] board,int row,int col){
+        for (int i=0;i<row;i++){
+            if (board[i][col]) return false;
+        }
+        int maxLeft=Math.min(row,col);
+        for (int i=1;i<=maxLeft;i++){
+            if (board[row-i][col-i]) return false;
+        }
+        int maxRight=Math.min(row,board.length-1-col);
+        for (int i=1;i<=maxRight;i++){
+            if (board[row-i][col+i]) return false;
+        }
+        return true;
+    }
+}
