20ab5292ab8b5bc20553090c78a114299199b15e
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-02-11 18:31:41 +0530

    Time: 10 ms (64.06%), Space: 50.3 MB (86.08%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,21 +1,62 @@
+class Pair{
+    int a;
+    int b;
+    public Pair(int _a,int _b){
+        this.a=_a;
+        this.b=_b;
+    }
+}
 class Solution {
     public int numEnclaves(int[][] grid) {
         int r=grid.length;
         int c=grid[0].length;
         boolean[][] visited=new boolean[r][c];
+        Queue<Pair> q=new LinkedList<Pair>();
         for (int i=0;i<c;i++){
-            if (visited[0][i]==false && grid[0][i]==1) dfs(grid,visited,0,i);
+            if (visited[0][i]==false && grid[0][i]==1){
+                q.add(new Pair(0,i));
+                visited[0][i]=true;
+            }
         }
         for (int i=0;i<c;i++){
-            if (visited[r-1][i]==false && grid[r-1][i]==1) dfs(grid,visited,r-1,i);
+            if (visited[r-1][i]==false && grid[r-1][i]==1){
+                q.add(new Pair(r-1,i));
+                visited[r-1][i]=true;
+            }
         }
         for (int i=0;i<r;i++){
-            if (visited[i][0]==false && grid[i][0]==1) dfs(grid,visited,i,0);
+            if (visited[i][0]==false && grid[i][0]==1){
+                q.add(new Pair(i,0));
+                visited[i][0]=true;
+            }
         }
         for (int i=0;i<r;i++){
-            if (visited[i][c-1]==false && grid[i][c-1]==1) dfs(grid,visited,i,c-1);
+            if (visited[i][c-1]==false && grid[i][c-1]==1){
+                q.add(new Pair(i,c-1));
+                visited[i][c-1]=true;
+            }
         }
         int res=0;
+        
+        
+        
+        while (!q.isEmpty()){
+            int pr=q.peek().a;
+            int pc=q.peek().b;
+            q.poll();
+            int[] delR={-1,0,1,0};
+            int[] delC={0,1,0,-1};
+            for (int i=0;i<4;i++){
+                int nR=pr+delR[i];
+                int nC=pc+delC[i];
+            if (nR>=0 && nC>=0 && nR<grid.length && nC<grid[0].length && visited[nR][nC]==false && grid[nR][nC]==1){
+                q.add(new Pair(nR,nC));
+                visited[nR][nC]=true;
+
+            }
+        }
+            
+        }        
         
         for (int i=0;i<r;i++){
             for (int j=0;j<c;j++){
@@ -26,14 +67,5 @@ class Solution {
         return res;
         
     }
-    void dfs(int[][] grid,boolean[][] visited,int r,int c){
-        visited[r][c]=true;
-        int[] delR={-1,0,1,0};
-        int[] delC={0,1,0,-1};
-        for (int i=0;i<4;i++){
-            int nR=r+delR[i];
-            int nC=c+delC[i];
-            if (nR>=0 && nC>=0 && nR<grid.length && nC<grid[0].length && visited[nR][nC]==false && grid[nR][nC]==1) dfs(grid,visited,nR,nC);
-        }
-    }
+   
 }
