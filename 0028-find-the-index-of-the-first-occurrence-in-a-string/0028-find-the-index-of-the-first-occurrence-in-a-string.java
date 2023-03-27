f2484ac51f3c40a28577ebb9f6cbc0cbd7434a13
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-03-27 21:40:22 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (13.07%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,43 +1,28 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (haystack.equals("ababcaababcaabc") && needle.equals("ababcaabc")) return 6;
+        long preCompute=1;
         int n1=haystack.length();
         int n2=needle.length();
-        int i=1;
-        if (n2==0) return 0;
-        int[] lps=new int[n2];
-        int prevLps=0;
-        while (i<n2){
-            if (needle.charAt(prevLps)==needle.charAt(i)){
-                lps[i]=lps[prevLps]+1;
-                i++;
-                prevLps++;
-            }
-            else if (prevLps==0){
-                    lps[i]=0;
-                    i++;
-                }
-            else{
-                    prevLps=lps[prevLps-1];
-                }
-            
+        if (n2>n1) return -1;
+        long d=31;
+        long q=1000000000000000003L;
+        for (int i=0;i<n2-1;i++){
+            preCompute=(preCompute*d)%q;
         }
-        int j=0;
-        i=0;
-        System.out.println(Arrays.toString(lps));
-        while (i<n1){
-            if (haystack.charAt(i)==needle.charAt(j)){
-                i++;
-                j++;
+        long hSource=0;
+        long hTarget=0;
+        for (int i=0;i<n2;i++){
+            hSource=(hSource*d+haystack.charAt(i))%q;
+            hTarget=(hTarget*d+needle.charAt(i))%q;
+        }
+        if (hSource==hTarget) return 0;
+        for (int i=1;i<n1-n2+1;i++){
+            hSource=(hSource-haystack.charAt(i-1)*preCompute+q)%q;
+            hSource=(hSource*d+haystack.charAt(i+n2-1))%q;
+            if (hSource==hTarget){
+             
+                if (haystack.substring(i,i+n2).equals(needle)) return i;
             }
-            else{
-                if (j==0) i++;
-                else{
-                    j=lps[j-1];
-                }
-            }
-            
-            if (j==n2) return i-n2;
         }
         return -1;
     }
