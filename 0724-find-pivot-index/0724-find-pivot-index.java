3ff12a144005bf64181dbececbcaf7f784e85373
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-04-06 19:25:10 +0530

    Time: 1 ms (100.00%), Space: 43.2 MB (61.16%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int lSum=0;
+        int rSum=0;
+        int l=nums.length;
+        for (int i=0;i<l;i++){
+            rSum+=nums[i];
+        }
+        int prev=0;
+        rSum-=nums[0];
+        int i=1;
+        if (rSum==0) return 0;
+        while (i<l){
+            lSum+=nums[prev];
+            rSum-=nums[i];
+            if (lSum==rSum) return i;
+            prev=i;
+            i++;
+        }
+        return -1;
+    }
+}
