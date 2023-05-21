c689ee9cb8b668af4371c6439f702b1eabebd5bd
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-05-21 18:12:37 +0530

    Time: 24 ms (89.48%), Space: 43.1 MB (15.96%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,22 +1,17 @@
 class Solution {
     public int numSquares(int n) {
      int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-       return recurse(n,dp);
+        for (int num=1;num<=n;num++){
+            int res=Integer.MAX_VALUE;
+           for (int i=1;i*i<=num;i++){
+             int s=i*i;
+             int ans=1+dp[num-s];
+             res=Math.min(res,ans);
+         }      
+        dp[num]=res;
+        }
+       return dp[n];
    
     }
-    
-     int recurse(int n,int[] dp){
-         if(n==0) return 0;
-         if (dp[n]!=-1) return dp[n];
-         int res=Integer.MAX_VALUE;
-         for (int i=1;i*i<=n;i++){
-             int s=i*i;
-             int ans=1+recurse(n-s,dp);
-             res=Math.min(res,ans);
-         }
-         dp[n]=res;
-         return res;
-         
-     }
+
 }
