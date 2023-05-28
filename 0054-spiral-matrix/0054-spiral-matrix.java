73f4578831b2b0d235fa4919f5c094e231d3aaa1
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-05-28 20:53:13 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (54.16%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,38 +1,31 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        // int[] res=new int[m*n];
-        List<Integer> res=new ArrayList<>();
-        recurse(0,0,m,n,res,matrix);
+        List<Integer> res=new ArrayList<>();        
+        recurse(0,0,matrix.length,matrix[0].length,res,matrix);
         return res;
         
         
     }
-    void recurse(int i,int j,int m,int n,List<Integer> arr,int[][] matrix){
+    void recurse(int i,int j,int m,int n,List<Integer> res,int[][] matrix){
         if (i>=m || j>=n) return;
         for (int p=i;p<n;p++){
-            arr.add(matrix[i][p]);
-          
+            res.add(matrix[i][p]);
         }
         for (int p=i+1;p<m;p++){
-            arr.add(matrix[p][n-1]);
-          
+            res.add(matrix[p][n-1]);
         }
-        if ((m-1)!=i){
-            for (int k=n-2;k>=j;k--){
-                arr.add(matrix[m-1][k]);
-               
+        if (i!=m-1){
+            for (int p=n-2;p>=j;p--){
+                res.add(matrix[m-1][p]);
             }
-            
         }
-        if ((n-1)!=j){
-            for (int k=m-2;k>i;k--){
-                arr.add(matrix[k][j]);
+       if (j!=n-1){
+            for (int p=m-2;p>i;p--){
+                res.add(matrix[p][j]);
             }
-            
         }
-        recurse(i+1,j+1,m-1,n-1,arr,matrix);
+    recurse(i+1,j+1,m-1,n-1,res,matrix);
+  
+  
     }
-    
 }
