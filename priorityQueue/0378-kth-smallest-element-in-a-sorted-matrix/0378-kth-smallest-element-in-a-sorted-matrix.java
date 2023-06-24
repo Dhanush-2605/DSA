de6f71348693a7d173cab2814c626e6fff8958da
dhanush <vellasiridhanush@gmail.com>
2023-06-24 20:36:35 +0530

    added problms

--- priorityQueue/0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ priorityQueue/0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,13 +1,17 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Integer> q=new PriorityQueue<>((a,b)->a-b);
-        for (int i=0;i<matrix.length;i++){
-            for (int j=0;j<matrix[0].length;j++){
-                q.add(matrix[i][j]);
-            }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int[] arr:matrix){
+            for (int val:arr){
+               pq.add(val);
         }
-        for (int i=0;i<k-1;i++) q.remove();
-        return q.remove();
+        }
+        int ans=0;
+        // System.out.println(pq);
+        for (int i=0;i<k;i++){
+            ans=pq.poll();            
+        }
+        return ans;
         
         
     }
