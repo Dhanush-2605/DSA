395d52ee4ecad26c83049da4c71635370bce19a3
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-07-20 20:07:14 +0530

    Time: 2 ms (99.69%), Space: 42.2 MB (93.74%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,31 +1,29 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
             List<List<Integer>> res=new ArrayList<>();
-        Arrays.sort(candidates);
-        List<Integer> temp1=new ArrayList<>();
-        recurse(0,candidates,res,temp1,target,0);
+             Arrays.sort(candidates);
+            List<Integer> temp1=new ArrayList<>();
+            recurse(0,candidates,res,temp1,target);
         return res;
         
     }
-    void recurse(int ind,int[] nums,List<List<Integer>> res,List<Integer> cur,int target,int sum){
-        if (sum>target) return;
+    void recurse(int ind,int[] nums,List<List<Integer>> res,List<Integer> cur,int target){   
   
-        if (ind>=nums.length){
-            if (sum== target){
-            List<Integer> temp=new ArrayList<>(cur);
-            res.add(temp);
-          
-        }   
+        if (target==0){
+              List<Integer> temp=new ArrayList<>(cur);
+              res.add(temp);          
               return;
         }
         
-      
-        cur.add(nums[ind]);
-        sum+=nums[ind];
-        recurse(ind+1,nums,res,cur,target,sum);
-        cur.remove(cur.size()-1);
-        sum-=nums[ind];
-        while (ind+1<nums.length && nums[ind]==nums[ind+1]) ind++;
-        recurse(ind+1,nums,res,cur,target,sum);
+      for (int i=ind;i<nums.length;i++){
+          if (i>ind && nums[i]==nums[i-1]) continue;
+          if (nums[i]>target) break;
+          cur.add(nums[i]);
+          recurse(i+1,nums,res,cur,target-nums[i]);        
+          cur.remove(cur.size()-1);
+        
+      }
+       return;
+    
     }
 }
