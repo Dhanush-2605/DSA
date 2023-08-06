729b48b5a8c04a8b832259f1af3ad925115cc654
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-08-06 19:01:16 +0530

    Time: 182 ms (5.03%), Space: 47.8 MB (84.51%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,23 +1,24 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        StringBuilder sb=new StringBuilder(s);
-        sb.reverse();
-        String rev=sb.toString();
-        int len=s.length();
-        int[][] dp=new int[len][len];
+         String res="";
+        for (char ch:s.toCharArray()){
+            res=ch+res;
+        }
+        int n=s.length();
+        int[][] dp=new int[n][n];
         for (int[] arr:dp) Arrays.fill(arr,-1);
-        return LCS(len-1,len-1,s,rev,dp);
+
+        return recurse(n-1,n-1,s,res,dp);
         
     }
-    int LCS(int i,int j,String s1,String s2,int[][] dp){
-        
+    int recurse(int i,int j,String s,String p,int[][] dp){
         if (i<0 || j<0) return 0;
         if (dp[i][j]!=-1) return dp[i][j];
-        int notTake=Math.max(LCS(i-1,j,s1,s2,dp),LCS(i,j-1,s1,s2,dp));
         int take=0;
-        if (s1.charAt(i)==s2.charAt(j)){
-            take=1+LCS(i-1,j-1,s1,s2,dp);
+        if (s.charAt(i)==p.charAt(j)){
+            take=1+recurse(i-1,j-1,s,p,dp);
         }
+        int notTake=Math.max(recurse(i-1,j,s,p,dp),recurse(i,j-1,s,p,dp));
         return dp[i][j]=Math.max(take,notTake);
     }
 }
