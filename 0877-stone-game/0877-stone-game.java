e2017d79f89140730076714c321bf204644a8111
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-08-19 10:18:42 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (98.11%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,36 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-     return true;   
+     boolean alice=true;
+     int n=piles.length;
+
+     int s=0;
+     int e=n-1;
+        int a=0;
+        int b=0;
+     while (e>=s){
+        if (alice){
+            if (piles[s]>=piles[e]){
+                a+=piles[s];
+                s++;
+            }
+            else{
+                a+=piles[e];
+                e--;
+            }
+            alice=false;
+        }
+         else{
+            if (piles[s]>=piles[e]){
+                b+=piles[e];
+                e--;
+            }
+            else{
+                b+=piles[s];
+                s++;
+            }
+             alice=true;
+         }
+    }
+        return (a>b);
     }
 }
