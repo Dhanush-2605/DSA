719896875cb26ba9f8f13460d35e40f8a0b66bf5
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-08-19 12:27:47 +0530

    Time: 5 ms (99.00%), Space: 55.3 MB (84.00%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,26 +1,19 @@
 class Solution {
-    public String stoneGameIII(int[] stoneValue) {
-        int n=stoneValue.length;
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        int ans=recurse(stoneValue,0,dp);
-
-        // System.out.println(ans);
-        if (ans>0) return "Alice";
-        if (ans<0) return "Bob";
+    public String stoneGameIII(int[] arr) {
+        int n=arr.length;
+        int[] dp=new int[n+1];
+        
+        for (int i=n-1;i>=0;i--){
+        int ans=Integer.MIN_VALUE;
+        ans=Math.max(ans,arr[i]-dp[i+1]);
+        if (i+1<arr.length) ans=Math.max(ans,arr[i]+arr[i+1]-dp[i+2]);
+        if (i+2<arr.length) ans=Math.max(ans,arr[i]+arr[i+1]+arr[i+2]-dp[i+3]);
+        dp[i]=ans;          
+        }
+        int an=dp[0];
+        if (an>0) return "Alice";
+        if (an<0) return "Bob";
         return "Tie";
     }
-    int recurse(int[] arr,int i,int[] dp){
-        if (i>=arr.length){
-            return 0;
-        }
-        if (dp[i]!=-1) return dp[i];
-        // else{
-        int ans=Integer.MIN_VALUE;
-        ans=Math.max(ans,arr[i]-recurse(arr,i+1,dp));
-        if (i+1<arr.length) ans=Math.max(ans,arr[i]+arr[i+1]-recurse(arr,i+2,dp));
-        if (i+2<arr.length) ans=Math.max(ans,arr[i]+arr[i+1]+arr[i+2]-recurse(arr,i+3,dp));
-        return dp[i]=ans;
-        // }
-    }
+  
 }
