4e19aabb6c358a2b3dcfce28235b2c67ed88848d
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-10-16 08:09:21 +0530

    Time: 1 ms (79.70%), Space: 40.5 MB (21.81%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,30 +1,19 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<int[]> rows=new ArrayList<>();
-        int[] cur=new int[1];
-        cur[0]=1;
-        rows.add(cur);
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> f1=new ArrayList<>();
+        f1.add(1);
+        res.add(f1);
         for (int i=1;i<=rowIndex;i++){
-            int[] prev=rows.get(rows.size()-1);
-            int l=0;
-            int r=1;
-            int[] newRow=new int[i+1];
-            newRow[0]=1;
-            newRow[newRow.length-1]=1;
-            int ind=1;
-            for (int j=0;j<(i-1);j++){
-                newRow[ind++]=prev[l]+prev[r];
-                l++;
-                r++;
-                
-                
+            List<Integer> cur=res.get(i-1);
+            List<Integer> temp=new ArrayList<>();
+            temp.add(1);
+            for (int j=0;j<cur.size()-1;j++){
+                temp.add(cur.get(j)+cur.get(j+1));
             }
-            rows.add(newRow);
+            temp.add(1);
+            res.add(temp);
         }
-                List<Integer> temp=new ArrayList<>();
-
-        for (int val:rows.get(rowIndex)) temp.add(val);
-        return temp;
-        
+        return res.get(rowIndex);
     }
 }
