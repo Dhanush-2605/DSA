c2ca55ea64e07e3da4a373cf90a11f60d2072628
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2023-10-20 16:28:11 +0530

    Time: 92 ms (13.78%), Space: 45.1 MB (6.16%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,18 +1,20 @@
 class Solution {
-    public int jump(int[] nums) {
-        int n=nums.length;
-        int l=0;
-        int r=0;
-        int res=0;
-        while (r<n-1){
-            int maxJump=0;
-            for (int i=l;i<=r;i++){
-                 maxJump=Math.max(maxJump,i+nums[i]);
-            }
-            l=r+1;
-            r=maxJump;
-            res++;
+    public int jump(int[] arr) {
+        int n=arr.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp,-1);
+        int ans=recurse(arr,0,dp);
+        if (ans==100000000) return -1;
+        return ans;
+    }
+    static int recurse(int[] arr,int ind,int[] dp){
+        if (ind>=arr.length-1) return 0;
+        if (dp[ind]!=-1) return dp[ind];
+        int ans=100000000;
+        for (int i=1;i<=arr[ind];i++){
+            int val=1+recurse(arr,ind+i,dp);
+            ans=Math.min(ans,val);
         }
-        return res;
+        return dp[ind]=ans;
     }
 }
