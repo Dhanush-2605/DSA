cf7189d4e6351ef479b6280a1d8f65a0861cc5bd
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2024-01-17 18:59:23 +0530

    Time: 32 ms (20.00%), Space: 45.5 MB (5.16%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,34 +1,36 @@
 class Solution {
-    Set<List<Integer>> set=new HashSet<>();
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        List<Integer> temp=new ArrayList<>();
-        for (int val:nums) temp.add(val);
-        permutation(temp,0);
-        List<List<Integer>> res=new ArrayList<>();
-        for (List<Integer> c:set) res.add(c);
-        return res;
+       Set<List<Integer>> set=new HashSet<>();
+
+    public List<List<Integer>> permuteUnique(int[] arr) {
+           List<Integer> cur=new ArrayList<>();
+
+           for (int val:arr) cur.add(val);
         
-        
-        
+    	    recurse(cur,0);
+            List<List<Integer>> ans=new ArrayList<>();
+
+
+    	   for (List<Integer> temp:set) ans.add(temp);
+
+    	    return ans;     
     }
-    
-    void swap(List<Integer> temp,int s,int e){
+  void swap(List<Integer> temp,int s,int e){
         int cur=temp.get(s);
         temp.set(s,temp.get(e));
         temp.set(e,cur);
     }
-    
-    void permutation(List<Integer> temp,int ind){
-        if (ind==temp.size()-1){
-            List<Integer> cur=new ArrayList<>(temp);
-            set.add(cur);
-            return;
-        }
-        for (int i=ind;i<temp.size();i++){
-            swap(temp,ind,i);
-            permutation(temp,ind+1);
-            swap(temp,ind,i);
-        }
-        return;
-    }
+    void recurse(List<Integer> arr,int ind){
+	    if (ind==arr.size()-1){
+	        List<Integer> temp=new ArrayList<>(arr);
+	        set.add(temp);
+	        return;
+	    }
+	    for (int i=ind;i<arr.size();i++){
+	        swap(arr,ind,i);
+	        recurse(arr,ind+1);
+            swap(arr,ind,i);
+	    }
+	    return;
+	}
 }
+
