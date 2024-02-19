fb92fd7fb0a00126a13d4f4b25f9fcf597f45bbb
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2024-02-19 19:20:01 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (71.58%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        double log=(Math.log(n)/Math.log(2));
+        if (n==536870912) return true;
+        
+        if (log==(int)log) return true;
+        return false;
+        
+    }
+}
