e732aa7f7e7f5c4404369643b76bba6b481f3719
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2024-02-26 09:22:33 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (30.60%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -13,16 +13,36 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public boolean isSameTree(TreeNode root1, TreeNode root2) {
-	   return dfs(root1,root2);
-	    
-	}
-	boolean dfs(TreeNode root,TreeNode root1){
-	    if (root==null && root1==null) return true;
-	    if ((root!=null && root1==null) || (root==null && root1!=null)) return false; 
-        if (root.val!=root1.val) return false;
-	 
-	    return dfs(root.left,root1.left) && dfs(root.right,root1.right);
-	}
+    boolean ans=true;
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return recurse(p,q);
+    }
+    boolean recurse(TreeNode p,TreeNode q){
+        if (p==null && q==null) return true;
+        if (p==null && q!=null){
+           return false; 
+        }
+        if (p!=null && q==null) return false;
+        if (p.val!=q.val) return false;
+
+        boolean l=recurse(p.left,q.left);
+        boolean r=recurse(p.right,q.right);
+        return (l && r);
+    }
 }
