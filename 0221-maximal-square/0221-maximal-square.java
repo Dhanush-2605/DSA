cd30ed5a89cb3a4a326b1d36a468d819d2616697
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2024-03-15 18:01:09 +0530

    Time: 9 ms (33.61%), Space: 57.3 MB (64.78%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -2,31 +2,31 @@ class Solution {
     public int maximalSquare(char[][] matrix) {
         int r=matrix.length;
         int c=matrix[0].length;
-        int[][] dp=new int[r][c];
-        int res=0;
-
+        int ans=0;
+        int[][] res=new int[r][c];
+        // for (int i=0;i<r;i++) res[i][0]=Character.getNumericValue(matrix[i][0]); 
+        // for (int i=0;i<c;i++) res[0][i]=Character.getNumericValue(matrix[0][i]);
+        // System.out.println(Arrays.deepToString(res));
         for (int i=0;i<r;i++){
-            dp[i][0]=Character.getNumericValue(matrix[i][0]);
-            res=Math.max(res,dp[i][0]);
-        }
-        for (int i=0;i<c;i++){
-            dp[0][i]=Character.getNumericValue(matrix[0][i]);
-            res=Math.max(res,dp[0][i]);
+            for (int j=0;j<c;j++){
 
-        }
-        for (int i=1;i<r;i++){
-            for (int j=1;j<c;j++){
-                if (matrix[i][j]=='0') dp[i][j]=0;
-                else{
-                    int n1=dp[i-1][j];
-                    int n2=dp[i][j-1];
-                    int n3=dp[i-1][j-1];
-                    dp[i][j]=1+Math.min(n1,Math.min(n2,n3));
-
-                    res=Math.max(res,dp[i][j]);
+               if (i==0 || j==0) res[i][j]=Character.getNumericValue(matrix[i][j]);
+               else{ 
+                    if (matrix[i][j]=='1')
+                    {
+                    
+                    res[i][j]=1+Math.min(res[i-1][j],Math.min(res[i][j-1],res[i-1][j-1]));
+                    }
+                   else res[i][j]=0;
                 }
+                                            ans=Math.max(ans,res[i][j]);
+
             }
-        }
-        return res*res;
+
+        } 
+        // System.out.println(Arrays.deepToString(res));
+
+        return ans*ans;
+
     }
 }
