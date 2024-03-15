7aa3bfcd9377b87eb9f5b9e438cbdcb4cb38283a
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2024-03-15 18:01:35 +0530

    Time: 32 ms (23.35%), Space: 45.9 MB (9.53%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,22 +1,24 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n=coins.length;
-        int[][] dp=new int[n][amount+1];
+        int[][] dp=new int[coins.length+1][amount+1];
         for (int[] arr:dp) Arrays.fill(arr,-1);
-        int ans=recurse(coins.length-1,coins,amount,dp);
-        if (ans==(int)Math.pow(10,9)) return -1;
+        int ans=recurse(coins,amount,coins.length-1,dp);
+
+        if (ans==Integer.MAX_VALUE-1) return -1;
         return ans;
-        
     }
-    int recurse(int ind,int[] coins,int amount,int[][] dp){
-        if (ind==0){
-            if (amount%coins[ind]==0) return amount/coins[ind];
-            else return (int)Math.pow(10,9);
+    int recurse(int[] coins,int amount,int i,int[][] dp){
+        if (amount==0) return 0;
+        if (amount<0 || i<0 ) return Integer.MAX_VALUE-1;
+        if (dp[i][amount]!=-1) return dp[i][amount];
+         int take=Integer.MAX_VALUE-1;
+        if (amount>=coins[i]){
+            take=1+recurse(coins,amount-coins[i],i,dp);
         }
-        if (dp[ind][amount]!=-1) return dp[ind][amount];
-        int notTake=recurse(ind-1,coins,amount,dp);
-        int take=Integer.MAX_VALUE;
-        if (amount>=coins[ind]) take=1+recurse(ind,coins,amount-coins[ind],dp);
-        return dp[ind][amount]=Math.min(take,notTake);
+        int notTake=recurse(coins,amount,i-1,dp);
+        
+         return dp[i][amount]=Math.min(take,notTake);
+
+
     }
 }
