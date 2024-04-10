11860a91c4a5f53ed013281cc018118d8dbcd0d5
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2024-04-10 22:06:19 +0530

    Time: 1 ms (99.43%), Space: 41.6 MB (88.78%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,23 +1,32 @@
 class Solution {
-    public boolean wordBreak(String S, List<String> wordDict) {
-    HashMap<String,Integer> map=new HashMap<>();
-        for (String s:wordDict){
-            map.put(s,1);
-        } 
-        int[] dp=new int[S.length()+1];
-        dp[dp.length-1]=1;
-        for (int ind=dp.length-2;ind>=0;ind--){
-           String temp="";
-            for (int i=ind;i<S.length();i++){
-                temp+=S.charAt(i);
-                if (map.containsKey(temp)){
-                        if (dp[i+1]==1) dp[ind]=1;;                        
-            }
-        }
-              
-        }
-        return dp[0]==1?true:false;
-     
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int[] dp=new int[s.length()];
+        Arrays.fill(dp,-1);
+
+        return recurse(s,wordDict,0,dp);
     }
- 
+    static boolean recurse(String s,List<String> dict,int ind,int[] dp){
+        if (ind>=s.length()) return true;
+        if (dp[ind]!=-1){
+            if (dp[ind]==0) return false;
+            return true;
+        }
+        boolean ans=false;
+
+        for (String cur:dict){
+
+            if (s.startsWith(cur,ind)){
+                ans=ans|| recurse(s,dict,ind+cur.length(),dp);
+            }
+
+
+
+
+        }
+        if (ans==false) dp[ind]=0;
+        else dp[ind]=1;
+
+        return ans;
+
+    }
 }
