a499a38471d164a2c8a1ed13e89630204f23bf09
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2024-05-28 18:04:01 +0530

    Time: 28 ms (83.92%), Space: 52.3 MB (26.91%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,41 +1,30 @@
 class Solution {
-
     public List<List<Integer>> threeSum(int[] nums) {
-            List<List<Integer>> ress=new ArrayList<>();
-             Arrays.sort(nums);
-        for (int i=0;i<nums.length;i++){
+        List<List<Integer>> res=new ArrayList<>();
+        Arrays.sort(nums);
+        int n=nums.length;
+        for (int i=0;i<n;i++){
             if (i>0 && nums[i]==nums[i-1]) continue;
-             
-            int l=i+1;
-            int r=nums.length-1;
-        while (r>l){
-            int sum=nums[i]+nums[l]+nums[r];
-            if (sum>0){
-                r-=1;
+            int j=i+1;
+            int k=n-1;
+            while (k>j){
+                int sum=nums[i]+nums[j]+nums[k];
+                if (sum<0) j++;
+                else if (sum>0) k--;
+                else{
+                    List<Integer> temp=new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    j++;
+                    k--;
+                    while (k>j && nums[j]==nums[j-1]) j++;
+                    while (k>j && nums[k]==nums[k+1]) k--;
+                    res.add(temp);
+                }
             }
-            else if (sum<0){
-                l+=1;
-            }
-            else{
-                List<Integer> res=new ArrayList<>();
-                res.add(nums[l]);
-                res.add(nums[r]);
-                res.add(nums[i]);
-                ress.add(res);
-               l++;
-                while (l<r && nums[l]==nums[l-1]){
-                    l++;
-                    }
-                
-                
-            }
-            
         }
-            
-        }
-        return ress;
+        return res;
         
     }
-
-    
 }
