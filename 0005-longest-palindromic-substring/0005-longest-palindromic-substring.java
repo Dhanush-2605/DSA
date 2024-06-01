cc9324a1ebb6dbb809513d1159b5e14fcff2ca1a
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2024-06-01 10:33:08 +0530

    Time: 123 ms (40.12%), Space: 45 MB (53.05%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,38 @@
 class Solution {
-    public String longestPalindrome(String s) {
-      int n=s.length();
-      int tempL=0;
-      String res="";
-    for (int i=0;i<n;i++){
-        int l=i;
-        int r=i;
-        while (l>=0 && r<n && s.charAt(l)==s.charAt(r)){
-            if ((r-l+1)>tempL){
-                tempL=r-l+1;
-                res=s.substring(l,r+1);
-            }
+    public String longestPalindrome(String st) {
+        int n=st.length();
+        boolean isEven=false;
+        String res="";
+        if (st.length()%2==0) isEven=true;
+        for (int i=0;i<n;i++){
+            int s=i;
+            int e=i;
+           
+                while (s>=0 && e<n && st.charAt(s)==st.charAt(e)){
+                    String sub=st.substring(s,e+1);
+                    
+                    if (sub.length()>res.length()) res=sub;
+                    s--;
+                    e++;
+                }
+                
+       
+       
+                s=i;
+                e=i+1;
+                while (s>=0 && e<n && st.charAt(s)==st.charAt(e)){
+                    String sub=st.substring(s,e+1);
+                    
+                    if (sub.length()>res.length()) res=sub;
+                    s--;
+                    e++;
+                }
+                
+        
             
-            l--;
-            r++;
+            
         }
-        l=i;
-        r=i+1;
-        while (l>=0 && r<n && s.charAt(l)==s.charAt(r)){
-            if ((r-l+1)>tempL){
-                tempL=r-l+1;
-                res=s.substring(l,r+1);
-            }
-            l--;
-            r++;
-        }
-    }
         return res;
-}
+        
+    }
 }
